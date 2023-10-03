7929e09624d40f2d00f445cdd67056a4da489109
Alexander  Petree <apetree1001@email.phoenix.edu>
2023-10-02 18:05:23 -0700

    Update humaneval.sh

--- evaluation/scripts/humaneval.sh
+++ evaluation/scripts/humaneval.sh
@@ -1,15 +1,20 @@
-cmd="accelerate launch eval.py \
-  --model replit/replit-code-v1-3b \
-  --tasks humaneval \
-  --temperature 0.2 \
-  --do_sample True \
-  --n_samples 1 \
-  --batch_size 10 \
-  --precision fp16 \
-  --allow_code_execution \
-  --trust_remote_code \
-  --save_generations \
-  --save_references"
-
-echo $cmd
-eval $cmd
+$cmd:
+-accelerate 
+-launch eval.py
+\--model 
+replit/replit
+-code-v1-3b 
+\--taskshumaneval
+\ --temperature 2 
+\ --do_sample
+--True\ --n_samples 1 
+\ --batch_size 10
+\--precision
+  -fp 8
+  \  --allow_code_execution 
+  \ --trust_remote_code
+  \ --save_generations
+  \--save_references"
+  $echo
+  $cmdeval
+  $cmd
